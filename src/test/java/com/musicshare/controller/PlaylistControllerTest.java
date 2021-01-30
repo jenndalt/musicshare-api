f75940a333195f68991c69dd10cbe5d6deb770fe
Parthiban Samykutti <parthiban.samykutti@gmail.com>
2021-01-30 12:57:19 -0500

    Added a new controller method createPlayList

--- src/test/java/com/musicshare/controller/PlaylistControllerTest.java
+++ src/test/java/com/musicshare/controller/PlaylistControllerTest.java
@@ -1,7 +1,38 @@
 package com.musicshare.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.musicshare.entity.Playlist;
+import com.musicshare.service.PlaylistService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {PlaylistController.class})
 class PlaylistControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockBean
+    private PlaylistService service;
+
+    @Test
+    public void testCreatePlaylist() throws Exception {
+        when(service.createPlaylist(any())).thenReturn(Playlist.builder().name("playList1").id(1l).songs(new ArrayList<>()).build());
+        ;
+        mockMvc.perform(post("/api/v1/playlist/{playListName}", "playList1")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.message").value("playlist created successfully."));
+        verify(service, times(1)).createPlaylist(any());
+    }
 }
