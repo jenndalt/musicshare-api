dd2ee937b644a10d86fbdc8687feb1fccf2831d4
Parthiban Samykutti <parthiban.samykutti@gmail.com>
2021-01-30 15:34:11 -0500

    Removed the print statement from the testcases. Added additional validation in the testcase  testCreatePlaylistWithName

--- src/test/java/com/musicshare/controller/PlaylistControllerTest.java
+++ src/test/java/com/musicshare/controller/PlaylistControllerTest.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,10 +29,12 @@ class PlaylistControllerTest {
     @DisplayName("createPlaylist - with a name")
     public void testCreatePlaylistWithName() throws Exception {
         when(service.createPlaylist(any())).thenReturn(Playlist.builder().name("playList1").id(1l).songs(new ArrayList<>()).build());
-        mockMvc.perform(post("/api/v1/playlist/{playListName}", "playList1") )
+        mockMvc.perform(post("/api/v1/playlist/{playListName}", "playList1"))
                 .andExpect(status().isOk())
-                .andDo(print())
                 .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").exists())
+                .andExpect(jsonPath("$.data.name").value("playList1"))
+                .andExpect(jsonPath("$.data.songs").isEmpty())
                 .andExpect(jsonPath("$.message").value("playlist created successfully."));
         verify(service, times(1)).createPlaylist(any());
     }
@@ -43,7 +44,6 @@ class PlaylistControllerTest {
     public void testCreatePlaylistWithoutName() throws Exception {
         mockMvc.perform(post("/api/v1/playlist/{playListName}", ""))
                 .andExpect(status().isOk())
-                .andDo(print())
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andExpect(jsonPath("$.message").value("playlist name is required."));
         verify(service, times(0)).createPlaylist(any());
@@ -54,7 +54,6 @@ class PlaylistControllerTest {
     public void testCreatePlaylistWithNameAsBlankSpace() throws Exception {
         mockMvc.perform(post("/api/v1/playlist/{playListName}", "   "))
                 .andExpect(status().isOk())
-                .andDo(print())
                 .andExpect(jsonPath("$.data").doesNotExist())
                 .andExpect(jsonPath("$.message").value("playlist name is required."));
         verify(service, times(0)).createPlaylist(any());
