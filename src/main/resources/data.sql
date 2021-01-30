c98c1b1993cbfaa96b2f4fea226002bf3862c202
Vyom <vyom.reloaded@gmail.com>
2021-01-30 16:44:28 -0500

    initialize the song table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO song (name) VALUES ('Toxic');
+INSERT INTO song (name) VALUES ('In the end');
+INSERT INTO song (name) VALUES ('Numb');
+INSERT INTO song (name) VALUES ('Vainko');
+INSERT INTO song (name) VALUES ('Cradles');
+INSERT INTO song (name) VALUES ('Highway to Hell');
